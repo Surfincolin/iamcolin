a0251c44e07bd07ab18a319605fba7bb78b05da8
Colin Wageman <colinwageman@gmail.com>
2017-11-05 11:43:07 -0800

    adding background video to home page

--- src/App.vue
+++ src/App.vue
@@ -66,7 +66,7 @@ export default {
 
 /* RESET */
 
-/* http://meyerweb.com/eric/tools/css/reset/ 
+/* http://meyerweb.com/eric/tools/css/reset/
    v2.0 | 20110126
    License: none (public domain)
 */
@@ -80,8 +80,8 @@ b, u, i, center,
 dl, dt, dd, ol, ul, li,
 fieldset, form, label, legend,
 table, caption, tbody, tfoot, thead, tr, th, td,
-article, aside, canvas, details, embed, 
-figure, figcaption, footer, header, hgroup, 
+article, aside, canvas, details, embed,
+figure, figcaption, footer, header, hgroup,
 menu, nav, output, ruby, section, summary,
 time, mark, audio, video {
   margin: 0;
@@ -92,7 +92,7 @@ time, mark, audio, video {
   vertical-align: baseline;
 }
 /* HTML5 display-role reset for older browsers */
-article, aside, details, figcaption, figure, 
+article, aside, details, figcaption, figure,
 footer, header, hgroup, menu, nav, section {
   display: block;
 }
@@ -274,7 +274,7 @@ hr {
 }
 
 .video-container {
-  padding-bottom: 2rem;
+  margin-bottom: 2rem;
   text-align: center;
 }
 
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,15 +3,16 @@
     <h2>Welcome, My name is Colin Wageman. I'm a creative developer who likes making interactive experiences. With a diverse background, I excel in looking at the bigger picture and determining the most efficient approach to situations.</h2>
     <div class="video-container">
       <!-- <a href="http://placeholder.com"><img src="http://via.placeholder.com/854x480?text=Video+Reel"></a> -->
-      <iframe src="https://player.vimeo.com/video/225743129" width="854" height="480" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
-      <!-- <video width="854" height="480" controls>
-        <source src="static/code-reel-v1.mp4" type="video/mp4">
+      <!-- <iframe src="https://player.vimeo.com/video/225743129" width="854" height="480" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe> -->
+      <video playsinline autoplay muted loop poster="polina.jpg" id="bgvid">
+        <source src="static/reel-bg-sm.mp4" type="video/mp4">
         Your browser does not support the video tag.
-      </video> -->
+      </video>
+      <div class="vid-overlay"></div>
     </div>
     <div class="standard-content">
       <h3>A Brief History</h3>
-      <p>After deciding traditional engineering wasn't for me I got into video production. As live streaming and webcasts became popular I started a live stream company. Over time I began developing custom servers and CDNs through AWS for productions. I ended up really enjoying the development and coding side of things, so I decided to pursue a career in software development. From there I worked with Sosolimited as a creative coder, until recently. Now I am freelancing and looking for opportunities to develop interactive applications and experiences.</p> 
+      <p>After deciding traditional engineering wasn't for me I got into video production. As live streaming and webcasts became popular I started a live stream company. Over time I began developing custom servers and CDNs through AWS for productions. I ended up really enjoying the development and coding side of things, so I decided to pursue a career in software development. From there I worked with Sosolimited as a creative coder, until recently. Now I am freelancing and looking for opportunities to develop interactive applications and experiences.</p>
       <h3 class="">Capabilities</h3>
       <ul class="capabilities">
         <li>C++</li>
@@ -55,6 +56,27 @@ h1, h2 {
   font-family: 'Source Sans Pro', sans-serif;
   letter-spacing: 0.4px;
   font-size: 1.9rem;
+}
+
+.video-container {
+  position: relative;
+}
+
+#bgvid {
+  width: 100%;
+  height: 100%;
+  z-index: 1;
+}
+
+.vid-overlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 99.5%;
+  /*background-image: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAYAAAAGCAYAAADgzO9IAAAAHUlEQVQYV2NkwAEYiZH4D1UEVoysA6cEiqk47QAAmZMCB/g/V3sAAAAASUVORK5CYII=);*/
+  background-image: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAL0lEQVQoU2NkIAzOMDAwmDASVgdRgawQrBOXRrJMxGYY3BaqmQi3Bd1EnB6ivtUA+lYEC0598BcAAAAASUVORK5CYII=);
+  z-index: 2;
 }
 
 .capabilities {
